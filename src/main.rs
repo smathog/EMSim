209001732e61ec7a96b39846690d57132d2acbd8
Scott Mathog <smathog@gmail.com>
2022-06-16 22:06:49 -0700

    Began creating utility vector generators

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use crate::election::ElectionMethods;
+use rand::thread_rng;
+use rand_distr::Beta;
 
 mod election;
 mod utility_generators;
-
-use utility_generators::foo;
 
 fn main() {
     println!("Number of ordinal methods currently implemented: {}", ElectionMethods::METHOD_COUNT_ordinal);
@@ -14,5 +14,20 @@ fn main() {
     ElectionMethods::METHOD_LIST_cardinal.iter()
         .enumerate()
         .for_each(|(i, s)| println!("Election Method (Cardinal) {}: {}", i, s));
-    foo();
+    let mut rng = thread_rng();
+    println!("UNIFORM UTILITY VECTORS");
+    for i in 0..10 {
+        println!("{:?}", utility_generators::uniform_utilities(&mut rng, 5))
+    }
+    println!("BETA a = b = 2");
+    let mut beta = Beta::new(2.0, 2.0).unwrap();
+    for i in 0..10 {
+        println!("{:?}", utility_generators::beta_utilities(beta, &mut rng, 5))
+    }
+
+    println!("BETA a = b = .5");
+    beta = Beta::new(0.5, 0.5).unwrap();
+    for i in 0..10 {
+        println!("{:?}", utility_generators::beta_utilities(beta, &mut rng, 5))
+    }
 }
--- src/utility_generators.rs
+++ src/utility_generators.rs
@@ -1,3 +1,22 @@
 //! mod to hold the various utility generators. Most are for generating the utility vecs for voters
 //! based on candidates
 
+use rand::Rng;
+use rand_distr::Beta;
+
+/// Given a number of candidates n, generate a utility vector of n elements where the utilities are
+/// chosen at random from the uniform distribution over [0, 1].
+pub fn uniform_utilities<T: Rng>(rng: &mut T, n: usize) -> Vec<f64> {
+    (0..n)
+        .map(|_| rng.gen::<f64>())
+        .collect()
+}
+
+/// Given a number of candidates n, generate a utility vector of n elements where the utilities are
+/// chosen at random from a Beta distribution over [0, 1]. The caller determines the alpha and beta
+/// parameters on the distribution.
+pub fn beta_utilities<T: Rng>(beta: Beta<f64>, rng: &mut T, n: usize) -> Vec<f64> {
+    (0..n)
+        .map(|_| rng.sample(beta))
+        .collect()
+}
